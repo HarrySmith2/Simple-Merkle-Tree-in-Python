75e098021fe47669a6b2e7ade99b6d7ffba97f3b
Harry Smith <33291554+HarrySmith2@users.noreply.github.com>
2018-05-19 19:54:53 +0100

    Updated to show how the program works step-by-step
    Added print statements as the algorithm goes through making the tree
    As well as adding a section at the end that proves the root hash is derived from input values in the transaction List

--- MerkleTrees.py
+++ MerkleTrees.py
@@ -3,142 +3,123 @@ import hashlib,json
 from collections import OrderedDict
 
 # 1. Declare the class trees
-class Jae_MerkTree:
+class MerkelTree:
 
-	# 2. Initiate the class object
-	def __init__(self,listoftransaction=None):
-		self.listoftransaction = listoftransaction
-		self.past_transaction = OrderedDict()
+    # 2. Initiate the class object
+    def __init__(self,listOfTransaction=None):
+        self.listOfTransaction = listOfTransaction
+        self.pastTransaction = OrderedDict()
 
-	# 3. Create the Merkle Tree  
-	def create_tree(self):
+    # 3. Create the Merkle Tree  
+    def createTree(self):
 
-		# 3.0 Continue on the declaration
-		listoftransaction = self.listoftransaction
-		past_transaction = self.past_transaction
-		temp_transaction = []
+        # 3.0 Continue on the declaration
+        listOfTransaction = self.listOfTransaction
+        pastTransaction = self.pastTransaction
+        tempTransaction = []
 
-		# 3.1 Loop until the list finishes
-		for index in range(0,len(listoftransaction),2):
+        # 3.1 For each transaction in list of Transactions
+        for index in range(0,len(listOfTransaction),2):
 
-			# 3.2 Get the most left element 
-			current = listoftransaction[index]
 
-			# 3.3 If there is still index left get the right of the left most element
-			if index+1 != len(listoftransaction):
-				current_right = listoftransaction[index+1]
+            # 3.2 Get the left most (last) element of transaction list
+            current = listOfTransaction[index]
+            print ("-"*10)
+            print ("Current Value = "+current)
+            
 
-			# 3.4 If we reached the limit of the list then make a empty string
-			else:
-				current_right = ''
+            # 3.3 If there is still an index left, get the right of the left most element
+            if index+1 != len(listOfTransaction):
+                currentRight = listOfTransaction[index+1]
+                
 
-			# 3.5 Apply the Hash 256 function to the current values
-			current_hash = hashlib.sha256(current)
+            # 3.4 If we reached the limit of the list then make a empty string
+            else:
+                currentRight = ''
+            print("current Right Value = " + currentRight)
+            
 
-			# 3.6 If the current right hash is not a '' <- empty string
-			if current_right != '':
-				current_right_hash = hashlib.sha256(current_right)
+            # 3.5 Apply sha256 Hash function to the current values
+            currentHash = hashlib.sha256((current).encode())
+            
 
-			# 3.7 Add the Transaction to the dictionary 
-			past_transaction[listoftransaction[index]] = current_hash.hexdigest()
+            # 3.6 If the current right hash is not a '' <- empty string
+            # 3.6.1 sha256 Hash the currentRight string.
+            if currentRight != '':
+                currentRightHash = hashlib.sha256((currentRight).encode())
 
-			# 3.8 If the next right is not empty
-			if current_right != '':
-				past_transaction[listoftransaction[index+1]] = current_right_hash.hexdigest()
+            # 3.7 Add the Transaction to the dictionary 
+            pastTransaction[listOfTransaction[index]] = currentHash.hexdigest()
+            print ("Current hash = "+ pastTransaction[listOfTransaction[index]])
 
-			# 3.9 Create the new list of transaction
-			if current_right != '':
-				temp_transaction.append(current_hash.hexdigest() + current_right_hash.hexdigest())
+            # 3.8 If the next right is not empty
+            if currentRight != '':
+                pastTransaction[listOfTransaction[index+1]] = currentRightHash.hexdigest()
+                print("Current Right Hash = "+ pastTransaction[listOfTransaction[index+1]])
 
-			# 3.01 If the left most is an empty string then only add the current value
-			else:
-				temp_transaction.append(current_hash.hexdigest())
+            # 3.9 Create the new list of transaction
+            if currentRight != '':
+                tempTransaction.append(currentHash.hexdigest() + currentRightHash.hexdigest())
 
-		# 3.02 Update the variables and rerun the function again 
-		if len(listoftransaction) != 1:
-			self.listoftransaction = temp_transaction
-			self.past_transaction = past_transaction
+            # 3.01 If the left most is an empty string then only add the current value
+            else:
+                tempTransaction.append(currentHash.hexdigest())
 
-			# 3.03 Call the function repeatly again and again until we get the root 
-			self.create_tree()
+        # 3.02 Update the variables and rerun the function again 
+        if len(listOfTransaction) != 1:
 
-	# 4. Return the past Transaction 
-	def Get_past_transacion(self):
-		return self.past_transaction
+            self.listOfTransaction = tempTransaction
+            self.pastTransaction = pastTransaction
 
-	# 5. Get the root of the transaction
-	def Get_Root_leaf(self):
-		last_key = self.past_transaction.keys()[-1]
-		return self.past_transaction[last_key]
+            # 3.03 Call the function repeatly again and again until we get the root 
+            self.createTree()
+
+    # 4. Return the past Transaction 
+    def getPastTransaction(self):
+        return self.pastTransaction
+
+    # 5. Get the root of the transaction
+    def getRootLeaf(self):
+        lastKey = list(self.pastTransaction.keys())[-1]
+        return self.pastTransaction[lastKey]
 
 # Declare the main part of the function to run
 if __name__ == "__main__":
 
-	# a) Create the new class of Jae_MerkTree
-	Jae_Tree = Jae_MerkTree()
+    # a) Create the new class of Jae_MerkTree
+    TxTree = MerkelTree()
 
-	# b) Give list of transaction
-	transaction = ['a','b','c','d']
+    # b) Give list of transaction
+    transaction = ['tx1','tx2','tx3','tx4']
 
-	# c) pass on the transaction list 
-	Jae_Tree.listoftransaction = transaction
+    # c) pass on the transaction list 
+    TxTree.listOfTransaction = transaction
 
-	# d) Create the Merkle Tree transaction
-	Jae_Tree.create_tree()
+    # d) Create the Merkle Tree transaction
+    TxTree.createTree()
 
-	# e) Retrieve the transaction 
-	past_transaction = Jae_Tree.Get_past_transacion()
+    # e) Retrieve the transaction 
+    pastTransaction = TxTree.getPastTransaction()
 
-	# f) Get the last transaction and print all 
-	print "First Example - Even number of transaction Merkel Tree"
-	print 'Final root of the tree : ',Jae_Tree.Get_Root_leaf()
-	print(json.dumps(past_transaction, indent=4))
-	print "-" * 50 
+    # f) Get the last transaction and print all 
+    print ("\n---- Proof Merkel Tree implementation is correct ----\n")
+    print ('Root(top) of the tree : ',TxTree.getRootLeaf())
 
-	# h) Second example
-	print "Second Example - Odd number of transaction Merkel Tree"
-	Jae_Tree = Jae_MerkTree()
-	transaction = ['a','b','c','d','e']
-	Jae_Tree.listoftransaction = transaction
-	Jae_Tree.create_tree()
-	past_transaction = Jae_Tree.Get_past_transacion()
-	print 'Final root of the tree : ',Jae_Tree.Get_Root_leaf()
-	print(json.dumps(past_transaction, indent=4))
-	print "-" * 50 
-
-	# i) Actual Use Case
-	print "Final Example - Actuall use case of the Merkle Tree"
-
-	# i-1) Declare a transaction - the ground truth
-	ground_truth_Tree = Jae_MerkTree()
-	ground_truth_transaction = ['a','b','c','d','e']
-	ground_truth_Tree.listoftransaction = ground_truth_transaction
-	ground_truth_Tree.create_tree()
-	ground_truth_past_transaction = ground_truth_Tree.Get_past_transacion()
-	ground_truth_root = ground_truth_Tree.Get_Root_leaf()
-
-	# i-2) Declare a tampered transaction
-	tampered_Tree = Jae_MerkTree()
-	tampered_Tree_transaction = ['a','b','c','d','f']
-	tampered_Tree.listoftransaction = tampered_Tree_transaction
-	tampered_Tree.create_tree()
-	tampered_Tree_past_transaction = tampered_Tree.Get_past_transacion()
-	tampered_Tree_root = tampered_Tree.Get_Root_leaf()
-
-	# i-3) The three company share all of the transaction 
-	print 'Company A - my final transaction hash : ',ground_truth_root
-	print 'Company B - my final transaction hash : ',ground_truth_root
-	print 'Company C - my final transaction hash : ',tampered_Tree_root
-
-	# i-4) Print out all of the past transaction
-	print "\n\nGround Truth past Transaction "
-	print(json.dumps(ground_truth_past_transaction, indent=4))
-	
-	print "\n\nTamper Truth past Transaction "
-	print(json.dumps(tampered_Tree_past_transaction, indent=4))
+    # -- Proof Merkel Tree implementation is correct --
+    
+    # g) Print hash value of tx1+tx2
+    print ("Hash of tx1 and tx2:")
+    print (pastTransaction[pastTransaction['tx1']+pastTransaction['tx2']])
+        # h) Print hash value of tx3+tx3
+    print ("Hash of tx3 and tx4:")
+    print (pastTransaction[pastTransaction['tx3']+pastTransaction['tx4']])
+    # j) Prints hash of tx1+tx2 and tx3+tx4
+    print ("Hash of root:")
+    rootValue = pastTransaction[pastTransaction[pastTransaction['tx1']+pastTransaction['tx2']]+pastTransaction[pastTransaction['tx3']+pastTransaction['tx4']]]
 
 
-
-
-
-# ---- END OF THE CODE ------
+    #print(json.dumps(pastTransaction, indent=4))
+    print ("Comparing root of tree hash to hash of (tx1+tx2) + (tx3+tx4)...")
+    if rootValue == TxTree.getRootLeaf():
+            print (True)
+    print ("-" * 50) 
